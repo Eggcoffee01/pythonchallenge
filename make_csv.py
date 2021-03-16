2e751bddb7ebf624842b8b4b7d341d9e22347860
Eggcoffee <76564967+Eggcoffee01@users.noreply.github.com>
2021-03-16 16:03:58 +0900

    Delete make_csv.py

--- make_csv.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-def makecsv(term, data):
-    f = open(f'{term}.csv', 'w', encoding='utf-8', newline='')
-    wr = csv.writer(f)
-    wr.writerow(['Title', 'Company', 'Link'])
-    for d in data:
-        wr.writerow([d['title'], d['company'], d['link']])
